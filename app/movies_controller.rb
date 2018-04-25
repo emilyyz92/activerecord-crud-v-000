ed6255345c6da14f10488ef90413fb8e4fc6a145
Emily Zhao <tong.zhao.92@gmail.com>
2018-04-25 20:08:02 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -37,7 +37,8 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last
+  movie_0 = Movie.last
+  movie_0.title
 end
 
 def can_get_size_of_the_database
